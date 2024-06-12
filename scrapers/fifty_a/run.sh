c3c2fa3b7c020338b5c4182aac9954868df798b3
Mike Yavorsky <mike.yavorsky@bioraft.com>
2024-06-11 20:12:37 -0400

    | run cpdp collection and copy to S3

--- scrapers/fifty_a/run.sh
+++ scrapers/fifty_a/run.sh
@@ -5,3 +5,6 @@ scrapy crawl officer -O officers.jsonl
 scrapy crawl command -O commands.jsonl
 aws s3 cp officers.jsonl s3://$BUCKET_NAME/officers/$(date +"%Y-%m-%dT%H-%M-%S")/50a.jsonl
 aws s3 cp commands.jsonl s3://$BUCKET_NAME/commands/$(date +"%Y-%m-%dT%H-%M-%S")/50a.jsonl
+cd ../
+python scrapers/citizens_police_data_project.py
+aws s3 cp ../data/citizens_police_data_project/$(date +"%Y-%m-%d")/officers/*.json s3://$BUCKET_NAME/officers/$(date +"%Y-%m-%dT%H-%M-%S")/cpdp/
