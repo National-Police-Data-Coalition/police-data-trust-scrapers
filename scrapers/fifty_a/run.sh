81dd408454b8e0976cf7c585e30ea9a7498c6abc
Armand <armasncode@gmail.com>
2024-07-21 13:06:17 -0400

    Updated run script to include downloading officers csv

--- scrapers/fifty_a/run.sh
+++ scrapers/fifty_a/run.sh
@@ -4,5 +4,6 @@ printf "Running 50A data collection\n"
 python fifty_a_download.py --officers-csv-filename=officers.csv
 scrapy crawl officer -O officers.jsonl
 scrapy crawl command -O commands.jsonl
+aws s3 cp officers.csv s3://$BUCKET_NAME/officers/$(date +"%Y-%m-%dT%H-%M-%S")/50a.csv
 aws s3 cp officers.jsonl s3://$BUCKET_NAME/officers/$(date +"%Y-%m-%dT%H-%M-%S")/50a.jsonl
 aws s3 cp commands.jsonl s3://$BUCKET_NAME/commands/$(date +"%Y-%m-%dT%H-%M-%S")/50a.jsonl
