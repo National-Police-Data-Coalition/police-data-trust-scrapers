0f448a723833384fb4e6453428fc54f9aacd5209
Armand <armasncode@gmail.com>
2024-07-21 17:33:59 -0400

    new command officer item to represent officer item under commmand model and additional attributes for command model

--- scrapers/fifty_a/fifty_a/items.py
+++ scrapers/fifty_a/fifty_a/items.py
@@ -3,12 +3,20 @@ from typing import List
 
 from scrapers.common.base_item import BaseItem
 
+@dataclass
+class CommandOfficerItem(BaseItem):
+    url: str
+    most_recent: int
 
 @dataclass
 class CommandItem(BaseItem):
     name: str
     url: str
-
+    website_url: str
+    commanding_officer_url: str
+    address: str
+    description: str
+    officers: List[CommandOfficerItem]
 
 @dataclass
 class OfficerItem(BaseItem):
